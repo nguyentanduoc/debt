585458f3db491fd26e803a888f37b19c64248092
ntduoc <tanduoc13@gmail.com>
2019-10-02 00:30:50 +0700

    first commit

---
+++ src/main/java/com/ntd/DebtManagement/DebtManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ntd.DebtManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtManagementApplication.class, args);
+    }
+
+}
